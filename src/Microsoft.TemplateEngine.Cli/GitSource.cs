62296e0e05f6b7d5e596183a14dfc828666d8464
Robb Schiefer <rschiefer@live.com>
2017-04-17 05:40:17 -0500

    added gitSource parse with tests

---
+++ src/Microsoft.TemplateEngine.Cli/GitSource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.TemplateEngine.Cli
+{
+    internal class GitSource
+    {
+        public string GitUrl { get; set; }
+        public string SubFolder { get; set; }
+
+        public GitSource(string gitUrl, string subFolder)
+        {
+            GitUrl = gitUrl;
+            SubFolder = subFolder;
+        }
+
+        public static bool TryParseGitSource(string spec, out GitSource package)
+        {
+            package = null;
+            int gitIndex = -1;
+
+            if (string.IsNullOrEmpty(spec) || (gitIndex = spec.IndexOf(".git", StringComparison.OrdinalIgnoreCase)) < 0)
+            {
+                return false;
+            }
+            else
+            {
+                var index = gitIndex + 4;
+                package = new GitSource(spec.Substring(0, index), spec.Substring(index));
+                return true;
+            }
+        }
+    }
+}
