502c69de4ece2691fee21e976eb67b63ddd43d74
Robb Schiefer <rschiefer@live.com>
2017-04-17 05:40:17 -0500

    added gitSource parse with tests

---
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/GitSourceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Core.UnitTests;
+using Microsoft.TemplateEngine.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Xunit;
+
+namespace Microsoft.TemplateEngine.Cli.UnitTests
+{
+    public class GitSourceTests : TestBase
+    {
+        [Theory(DisplayName = nameof(GitUrlReturnsGitPackageWithUrl))]
+        [InlineData("https://github.com/acme/templates.git/sub/folder", "https://github.com/acme/templates.git")]
+        [InlineData("https://github.com/acme/templates.git", "https://github.com/acme/templates.git")]
+        [InlineData("https://github.com/acme/templates.GIT", "https://github.com/acme/templates.GIT")]
+        public void GitUrlReturnsGitPackageWithUrl(string request, string gitUrl)
+        {
+            GitSource source = null;
+            var result = GitSource.TryParseGitSource(request, out source);
+
+            Assert.Equal(gitUrl, source.GitUrl);
+            Assert.Equal(true, result);
+        }
+        [Theory(DisplayName = nameof(NonGitUrlReturnsFalseNull))]
+        [InlineData("C:\\temp\\MyTemplate")]
+        [InlineData("Nuget::1")]
+        [InlineData(null)]
+        [InlineData("")]
+        public void NonGitUrlReturnsFalseNull(string request)
+        {
+            GitSource source = null;
+            var result = GitSource.TryParseGitSource(request, out source);
+
+            //IReadOnlyList<string> projFilesFound = actionProcessor.FindProjFileAtOrAbovePath(EngineEnvironmentSettings.Host.FileSystem, outputBasePath, new HashSet<string>());
+            Assert.Equal(null, source);
+            Assert.Equal(false, result);
+        }
+    }
+}
