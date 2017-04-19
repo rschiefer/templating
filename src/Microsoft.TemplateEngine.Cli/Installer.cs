f39087ecfb0604aa0b156e6166b83052c764bcf5
Robb Schiefer <rschiefer@live.com>
2017-04-18 21:59:49 -0500

    added installer tests and git source logic

--- src/Microsoft.TemplateEngine.Cli/Installer.cs
+++ src/Microsoft.TemplateEngine.Cli/Installer.cs
@@ -10,6 +10,7 @@ using Microsoft.TemplateEngine.Abstractions;
 using Microsoft.TemplateEngine.Abstractions.Mount;
 using Microsoft.TemplateEngine.Edge;
 using Microsoft.TemplateEngine.Edge.Settings;
+using System.Diagnostics;
 
 namespace Microsoft.TemplateEngine.Cli
 {
@@ -28,6 +29,7 @@ namespace Microsoft.TemplateEngine.Cli
         {
             List<string> localSources = new List<string>();
             List<Package> packages = new List<Package>();
+            List<GitSource> gitSources = new List<GitSource>();
 
             foreach (string request in installationRequests)
             {
@@ -45,6 +47,10 @@ namespace Microsoft.TemplateEngine.Cli
                 {
                     packages.Add(package);
                 }
+                else if (GitSource.TryParseGitSource(req, out GitSource gitSource))
+                {
+                    gitSources.Add(gitSource);
+                }
                 else
                 {
                     localSources.Add(request);
@@ -59,6 +65,11 @@ namespace Microsoft.TemplateEngine.Cli
             if (packages.Count > 0)
             {
                 InstallRemotePackages(packages);
+            }
+
+            if (gitSources.Count > 0)
+            {
+                InstallGitSources(gitSources);
             }
 
             _environmentSettings.SettingsLoader.Save();
@@ -189,6 +200,23 @@ namespace Microsoft.TemplateEngine.Cli
             InstallLocalPackages(newLocalPackages);
         }
 
+        private void InstallGitSources(List<GitSource> gitSources)
+        {
+            var tempPath = ".templating-temp";
+            _paths.CreateDirectory(tempPath);
+
+            var newLocalPackages = new List<string>();
+
+            foreach (var source in gitSources)
+            {
+                ExecuteProcess("git", "clone", source.GitUrl);
+            }
+
+            InstallLocalPackages(newLocalPackages);
+
+            _paths.DeleteDirectory(tempPath);
+        }
+
         private void InstallLocalPackages(IReadOnlyList<string> packageNames)
         {
             List<string> toInstall = new List<string>();
@@ -236,6 +264,26 @@ namespace Microsoft.TemplateEngine.Cli
                     _environmentSettings.Host.OnNonCriticalError("InvalidPackageSpecification", string.Format(LocalizableStrings.BadPackageSpec, pkg), null, 0);
                 }
             }
+        }
+
+
+        internal virtual bool ExecuteProcess(string command, params string[] args)
+        {
+
+            var info = new ProcessStartInfo
+            {
+                FileName = command,
+                Arguments = string.Join(" ", args)
+            };
+            Process p = Process.Start(info);
+            p.BeginOutputReadLine();
+            p.BeginErrorReadLine();
+            //p.ErrorDataReceived += OnErrorDataReceived;
+            //p.OutputDataReceived += OnOutputDataReceived;
+            p.WaitForExit();
+
+            //return new Result(_stdout?.ToString(), _stderr?.ToString(), p.ExitCode);
+            return p.ExitCode == 0;
         }
     }
 }
--- test/Microsoft.TemplateEngine.Cli.UnitTests/InstallerTests.cs
+++ test/Microsoft.TemplateEngine.Cli.UnitTests/InstallerTests.cs
@@ -1,29 +1,52 @@
-﻿using Microsoft.TemplateEngine.Core.UnitTests;
+﻿using Microsoft.TemplateEngine.Abstractions;
+using Microsoft.TemplateEngine.Core.UnitTests;
 using Microsoft.TemplateEngine.Utils;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Linq;
 using Xunit;
 
 namespace Microsoft.TemplateEngine.Cli.UnitTests
 {
     public class InstallerTests : TestBase
     {
-        [Fact(DisplayName = nameof(GitTemplatePathCallsGitClone))]
-        public void GitTemplatePathCallsGitClone()
+        [Theory(DisplayName = nameof(GitTemplatePathCallsGitClone))]
+        [InlineData("https://github.com/acme/templates.git/sub/folder", "https://github.com/acme/templates.git")]
+        [InlineData("https://github.com/acme/templates.git", "https://github.com/acme/templates.git")]
+        public void GitTemplatePathCallsGitClone(string request, string gitUrl)
         {
             //string targetBasePath = FileSystemHelpers.GetNewVirtualizedPath(EngineEnvironmentSettings);
             //string projFileFullPath = Path.Combine(targetBasePath, "MyApp.proj");
             //EngineEnvironmentSettings.Host.FileSystem.WriteAllText(projFileFullPath, TestCsprojFile);
-            
-            IInstaller installer = new Installer(this.EnvironmentSettings);
 
-            var installationRequests = new [] { "https://github.com/sayedihashimi/dotnet-new-samples.git/" };
+            InstallerTestWrapper installer = new InstallerTestWrapper(this.EnvironmentSettings);
+
+            var installationRequests = new [] { request };
             installer.InstallPackages(installationRequests);
 
             //IReadOnlyList<string> projFilesFound = actionProcessor.FindProjFileAtOrAbovePath(EngineEnvironmentSettings.Host.FileSystem, outputBasePath, new HashSet<string>());
-            //Assert.Equal(1, projFilesFound.Count);
+            Assert.Equal("git", installer.ExecuteProcessCommands[0][0]);
+            Assert.Equal("clone", installer.ExecuteProcessCommands[0][1]);
+            Assert.Equal(gitUrl, installer.ExecuteProcessCommands[0][2]);
+        }
+    }
+
+    internal class InstallerTestWrapper : Installer
+    {
+        public InstallerTestWrapper(IEngineEnvironmentSettings environmentSettings) : base(environmentSettings)
+        {
+            ExecuteProcessCommands = new List<string[]>();
+        }
+
+        public List<string[]> ExecuteProcessCommands { get; set; }
+        public bool ExecuteProcessReturn { get; set; }
+
+        internal override bool ExecuteProcess(string command, params string[] args)
+        {
+            ExecuteProcessCommands.Add(new string[] { command }.Concat(args).ToArray());
+            //base.ExecuteProcess(command, args);
+            return ExecuteProcessReturn;
         }
     }
 }
